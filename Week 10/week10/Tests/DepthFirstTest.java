151d6d565128ef93d16a0ca963fa2a0c2b622ff6
J.Pritchard U1661665 <U1661665@unimail.hud.ac.uk>
2018-12-03 15:10:14 +0000

    Week 10 work : 03.12.2018

--- Week 10/week10/Tests/DepthFirstTest.java
+++ Week 10/week10/Tests/DepthFirstTest.java
@@ -1,4 +1,10 @@
 import graph.AdjacencyGraph;
+import graph.DepthFirstTraversal;
+import graph.GraphError;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
 
 /**
  * Created by u1661665(Joshua Pritchard) on 30/11/2018.
@@ -6,5 +12,46 @@ import graph.AdjacencyGraph;
  */
 public class DepthFirstTest<T> extends AdjacencyGraph<T>
 {
+
+    @Test
+    void test() throws GraphError
+    {
+        DepthFirstTraversal<Integer> intGraph = new DepthFirstTraversal<>();
+
+        intGraph.add(1);
+        intGraph.add(2);
+        intGraph.add(3);
+
+        intGraph.add(1, 2);
+        intGraph.add(2, 3);
+        intGraph.add(3, 1);
+
+        ArrayList<Integer> returnedList = (ArrayList<Integer>) intGraph.traverse();
+
+        if (returnedList.get(0) == 1 && returnedList.get(1) == 2 && returnedList.get(2) == 3)
+        {
+            return;
+        }
+        else if (returnedList.get(1) == 1 && returnedList.get(2) == 2 && returnedList.get(0) == 3)
+        {
+            return;
+        }
+        else if(returnedList.get(2) == 1 && returnedList.get(0) == 2 && returnedList.get(1) == 3)
+        {
+            return;
+        }
+        else
+        {
+            fail("Correct path not returned.");
+        }
+    }
+
+    /*
+    circular graph test.
+    think of simple graph structures.
+        3 sets of interconnected node pairs.
+    graph structures with easy testable properties.
+    also put in doc about challenging to test this.
+     */
 
 }
--- Week 10/week10/src/graph/DepthFirstTraversal.java
+++ Week 10/week10/src/graph/DepthFirstTraversal.java
@@ -8,55 +8,91 @@ import java.util.*;
  */
 public class DepthFirstTraversal<T> extends AdjacencyGraph<T> implements Traversal<T>
 {
-    ArrayList<T> visited;
-    LinkedList<T> toDo;
+    /**
+     * Used to hold the list in which the nodes of the graph are traversed.
+     */
     ArrayList<T> traversal;
-    ArrayList<T> nodes;
 
+    /**
+     * A constructor for a depth first traversal that initializes the data structure used for the traversal list.
+     */
     public DepthFirstTraversal()
     {
-        visited = new ArrayList<>();
-        toDo = new LinkedList<>();
         traversal = new ArrayList<>();
-        nodes = new ArrayList<>();
     }
 
 
+    /**
+     * An implementation of the traverse() method defined in Traversal.
+     *
+     * @return Returns a list of nodes in the order of which they were found during traversal.
+     * @throws GraphError if the graph given to the traversal is empty.
+     */
     @Override
     public List<T> traverse() throws GraphError
     {
+        //This is used to ensure the existence of at least one node.
         if(getNoOfNodes() == 0)
-        {
             throw new GraphError("Graph is empty.");
-        }
 
-        for(T obj: getNodes())
+
+        //At least one node exists so this can be used safely.
+        T node = getUnvisitedNode();
+        do
         {
-            nodes.add(obj);
-        }
+            //Begin the population of the traversal list from the chosen node.
+            populateTraversal(getUnvisitedNode());
 
-        populateTraversal(nodes.get(0));
+            //Check for any more unvisited nodes.
+            node = getUnvisitedNode();
 
+        //Repeat if another unvisited node was found after the first pass.
+        }while(node != null);
+
+        //Return the now populated traversal list.
         return traversal;
     }
 
+    /**
+     * A recursive method to find and populate the traversal list for graph based on the initial T node.
+     *
+     * @param node the node to begin the traversal and search from.
+     * @throws GraphError
+     */
     private void populateTraversal(T node) throws GraphError
     {
-        traversal.add(node);
-
-        if(!visited.contains(node))
+        //If the node has not already been visited.
+        if(!traversal.contains(node))
         {
-            visited.add(node);
+            //Record the node as visited and add it to the traversal list.
+            traversal.add(node);
+
+            //For each of its neighbours, run this method again.
             for(T neighbour:getNeighbours(node))
             {
-                toDo.add(neighbour);
+                populateTraversal(neighbour);
             }
-
         }
+    }
 
-        while(toDo.size() > 0)
+    /**
+     * Get an unvisited node from the graphs set of nodes.
+     *
+     * @return a T node within the graph that has been unvisited according to the traversal list in this class. OR null
+     * if no unvisited node has been found./
+     * if no unvisited node has been found.
+     */
+    private T getUnvisitedNode()
+    {
+        //For each node in the set of nodes.
+        for(T node : getNodes())
         {
-            populateTraversal(toDo.poll());
+            //If it is unvisited, return this node.
+            if(!traversal.contains(node))
+                return node;
         }
+
+        //Default case for when no node is found.
+        return null;
     }
 }
