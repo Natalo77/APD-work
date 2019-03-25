888c137cbe80f939578b5e6f448e0f50df638a21
J.Pritchard U1661665 <U1661665@unimail.hud.ac.uk>
2019-03-25 15:07:37 +0000

    Week 21 work started. Justifications need to be proving from prior justifications

--- Week_21/week21/src/arithmetic/Cube.java
+++ Week_21/week21/src/arithmetic/Cube.java
@@ -1,48 +1,48 @@
 package arithmetic;
 
-import javax.swing.JOptionPane;
+		import javax.swing.JOptionPane;
 
 public class Cube {
 	public static int cube(int n) {
-		  int cube = 0, threeNsquared = 0, threeN = 0;
+		int cube = 0, threeNsquared = 0, threeN = 0;
 
-		  int i = 0;
+		int i = 0;
 
-          assert cube == i*i*i : cube;
-          assert threeNsquared == 3*i*i : threeNsquared;
-          assert threeN == 3*i : threeN;
+		assert cube == i*i*i : cube;
+		assert threeNsquared == 3*i*i : threeNsquared;
+		assert threeN == 3*i : threeN;
 
-		  while (i < n) {
-		      assert cube == i*i*i : cube;
-		      assert threeN == 3*i : threeN;
-		      assert threeNsquared == 3*i*i : threeNsquared;
+		while (i < n) {
+			assert cube == i*i*i : cube;
+			assert threeN == 3*i : threeN;
+			assert threeNsquared == 3*i*i : threeNsquared;
 
-			  cube = cube + threeNsquared + threeN + 1;
-              assert cube == (i+1)*(i+1)*(i+1) : cube;
-              assert threeN == 3*i : threeN;
-              assert threeNsquared == 3*i*i : threeNsquared;
+			cube = cube + threeNsquared + threeN + 1;
+			assert cube == (i+1)*(i+1)*(i+1) : cube;
+			assert threeN == 3*i : threeN;
+			assert threeNsquared == 3*i*i : threeNsquared;
 
-			  threeNsquared = threeNsquared + 2*threeN + 3;
-              assert cube == (i+1)*(i+1)*(i+1) : cube;
-              assert threeN == 3*i : threeN;
-              assert threeNsquared == 3*(i+1)*(i+1) : threeNsquared;
+			threeNsquared = threeNsquared + 2*threeN + 3;
+			assert cube == (i+1)*(i+1)*(i+1) : cube;
+			assert threeN == 3*i : threeN;
+			assert threeNsquared == 3*(i+1)*(i+1) : threeNsquared;
 
-			  threeN = threeN + 3;
-			  assert cube == (i+1)*(i+1)*(i+1) : cube;
-			  assert threeN == 3*(i+1) : threeN;
-			  assert threeNsquared == 3*(i+1)*(i+1) : threeNsquared;
+			threeN = threeN + 3;
+			assert cube == (i+1)*(i+1)*(i+1) : cube;
+			assert threeN == 3*(i+1) : threeN;
+			assert threeNsquared == 3*(i+1)*(i+1) : threeNsquared;
 
-			  i++;
-			  assert cube == i*i*i : cube;
-              assert threeN == 3*(i) : threeN;
-              assert threeNsquared == 3*(i)*(i) : threeNsquared;
-		  }
-		  assert i == n : i;
-		  assert cube == i*i*i : cube;
-		  assert cube == n*n*n : cube;
-		  return cube;
+			i++;
+			assert cube == i*i*i : cube;
+			assert threeN == 3*(i) : threeN;
+			assert threeNsquared == 3*(i)*(i) : threeNsquared;
+		}
+		assert i == n : i;
+		assert cube == i*i*i : cube;
+		assert cube == n*n*n : cube;
+		return cube;
 	}
-	
+
 	public static void main(String[] args) {
 		int x = ReadInt.read("Please input a number to be cubed");
 		int result = cube(x);
